4c31ebfa9eb60ca367b2e75fd1fff44689a0f836
Jacob Childress <jacobc@gmail.com>
2013-05-13 16:38:03 -0500

    Added support for setting temperature and away mode. Also added support for getting leaf status.

--- lib/siriproxy-nest.rb
+++ lib/siriproxy-nest.rb
@@ -1,12 +1,13 @@
 require 'nest_thermostat'
 
 class SiriProxy::Plugin::Nest < SiriProxy::Plugin
+
+  LOWER_LIMIT = 50
+  UPPER_LIMIT = 90
+
   attr_reader :email
   attr_reader :password
   attr_reader :nest
-
-  patterns = [ "house %s", "inside %s", "%s in here", "%s inside" ]
-  stats = [ "temperature", "humidity" ]
 
   def initialize(config)
     @email = config["email"]
@@ -19,21 +20,89 @@ class SiriProxy::Plugin::Nest < SiriProxy::Plugin
 
   end
 
-  patterns.map do |pattern|
+  read_patterns = [ "house %s", "inside %s", "%s in here", "%s inside" ]
+
+  read_patterns.map do |pattern|
     command = pattern % [ "temperature" ]
     listen_for(/#{command}/i) { respond_with_temperature }
     command = pattern % [ "humidity" ]
     listen_for(/#{command}/i) { respond_with_humidity }
   end
 
+  listen_for(/leaf status/i) { respond_with_leaf_status }
+
+  set_patterns = [ "set the temperature to",
+                   "set the thermostat to",
+                   "set the Nest to" ]
+  set_patterns.map do |pattern|
+    listen_for /#{pattern} ([0-9,]*[0-9])/i do |temperature|
+      set_temperature temperature
+      request_completed
+    end
+  end
+
+  [ "I'm leaving", "we're leaving",
+    "set the thermostat to away", 
+    "set nest to away", "set away", "set to away" ].map do |command|
+    listen_for(/#{command}/i) { set_away_mode }
+  end
+
+  [ "I'm back", "we're back", 
+    "I'm home", "we're home",
+    "stop away mode", "cancel away mode" ].map do |command|
+    listen_for(/#{command}/i) { end_away_mode }
+  end
+
   def respond_with_temperature
     temp = @nest.current_temperature.round()
     say "The house temperature is #{temp} degrees."
+    request_completed
   end
 
   def respond_with_humidity
     humidity = @nest.humidity.round()
     say "The humidity is #{humidity} percent."
+    request_completed
+  end
+
+  def respond_with_leaf_status
+    leaf_status = @nest.leaf
+    if leaf_status
+      say "You have a leaf. Good for you!"
+    else
+      say "You don't have a leaf right now. Try to save energy!"
+    end
+    request_completed
+  end
+
+  def is_numeric?(object)
+    true if Float(object) rescue false
+  end
+
+  def set_temperature(temperature)
+    if is_numeric?(temperature)
+      if temperature.to_i < LOWER_LIMIT or temperature.to_i > UPPER_LIMIT
+        say "I'm sorry. I can't do that."
+      else
+        @nest.temperature = temperature
+        say "I've set the thermostat to #{temperature} degrees."
+      end
+    else
+      say "That's not a temperature."
+    end
+    request_completed
+  end
+
+  def set_away_mode
+    @nest.away = true
+    say "The Nest is now in away mode."
+    request_completed
+  end
+
+  def end_away_mode
+    @nest.away = false
+    say "The Nest is now not in away mode."
+    request_completed
   end
 
 end
